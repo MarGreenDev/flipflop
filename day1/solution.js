e4127d8a22c4dc94f5fd276b8bf1d1c266d23859
Marylou Groeneveld <marylougroeneveld@gmail.com>
2026-07-07 21:32:49 +0200

    day 1 part 2

--- day1/solution.js
+++ day1/solution.js
@@ -6,11 +6,18 @@ const lines = input.split("\n");
 let warmCoffee = 0;
 let coldCoffee = 0;
 let totalSeconds = 0;
+let totalDegrees = 0;
+let totalTime = 0;
 
 for (const line of lines) {
     const temp = Number(line)
-    if (temp >= 60) {
+    if (temp > 60) {
         warmCoffee++;
+
+        const degrees = temp - 60;
+        totalDegrees += degrees;
+
+        totalTime += degrees * 5;
     }
     else if (temp < 60) {
         coldCoffee++;
@@ -21,6 +28,12 @@ for (const line of lines) {
     }
 }
 
+let totalTotalTime = 0;
+
+totalTotalTime = totalTime + totalSeconds;
+
 console.log("warm: " + warmCoffee);
 console.log("cold: " + coldCoffee);
 console.log(totalSeconds);
+
+console.log (totalTotalTime);
