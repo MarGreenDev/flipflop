fb53b2a9063dd2d03de89d619f1c552275d3d993
MarGreenDev <marylougroeneveld@gmail.com>
2026-07-11 15:06:59 +0200

    solve puzzle 3 part 2

--- puzzle3/solution.js
+++ puzzle3/solution.js
@@ -10,29 +10,58 @@ let highestStrenght = 0;
 let strongestPassword = "";
 
 for (let i = 0; i < passwords.length; i++) {
-    passwordPoints.push(0);
-    passwordLengths.push(0);
-    passwordStrengths.push(0);
+  passwordPoints.push(0);
+  passwordLengths.push(0);
+  passwordStrengths.push(0);
 
-    if (/[a-z]/.test(passwords[i])) {
-        passwordPoints[i]++;
+  if (/[a-z]/.test(passwords[i])) {
+    passwordPoints[i]++;
+  }
+  if (/[A-Z]/.test(passwords[i])) {
+    passwordPoints[i]++;
+  }
+  if (/[0-9]/.test(passwords[i])) {
+    passwordPoints[i]++;
+  }
+
+  //part 2
+
+  if (/[7]/.test(passwords[i])) {
+    if (!/[0-68-9]/.test(passwords[i])) {
+      passwordPoints[i] += 7;
     }
-    if (/[A-Z]/.test(passwords[i])) {
-        passwordPoints[i]++;
+  }
+
+  let currentRun = 1;
+  let longestRun = 1;
+
+  for (let j = 1; j < passwords[i].length; j++) {
+    if (passwords[i][j] === passwords[i][j - 1]) {
+      currentRun++;
+    } else {
+      currentRun = 1;
     }
-    if (/[0-9]/.test(passwords[i])) {
-        passwordPoints[i]++;
+    if (currentRun > longestRun) {
+      longestRun = currentRun;
     }
+}
 
-    passwordLengths[i] = passwords[i].length;
+if (longestRun >= 3) {
+  passwordPoints[i] += longestRun * longestRun;
+}
 
-    passwordStrengths[i] = passwordPoints[i] * passwordLengths[i];
+  passwordLengths[i] = passwords[i].length;
 
-    if (passwordStrengths[i] > highestStrenght) {
-        highestStrenght = passwordStrengths[i]
-        strongestPassword = passwords[i];
-    }
+  passwordStrengths[i] = passwordPoints[i] * passwordLengths[i];
 
+  if (/red|green|blue/.test(passwords[i])) {
+    passwordStrengths[i] *= 3;
+  }
+
+  if (passwordStrengths[i] > highestStrenght) {
+    highestStrenght = passwordStrengths[i];
+    strongestPassword = passwords[i];
+  }
 }
 
 console.log(strongestPassword);
